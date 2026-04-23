41a05687c883e177ba0aa2c9bb7c8f3f19167c3f
George Spanos <gespa11019@gmail.com>
2026-04-23 13:56:54 +0300

    walkable and direction functions

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -1,4 +1,14 @@
 module GameLogic where
 
-import Types as T
+import Types
 
+walkable :: Tile -> Bool
+walkable Floor = True
+walkable Door = True
+walkable Wall = False
+
+dirVector :: Direction -> (Int, Int)
+dirVector DirRight = (1, 0)
+dirVector DirLeft = (-1, 0)
+dirVector DirUp = (0, 1)
+dirVector DirDown = (0, -1)
--- src/Types.hs
+++ src/Types.hs
@@ -4,7 +4,3 @@ data Tile = Wall | Floor | Door deriving (Eq, Show)
 
 data Direction = DirUp | DirDown | DirRight | DirLeft deriving (Eq, Show)
 
-walkable :: Tile -> Bool
-walkable t = case t of
-  Wall -> False
-  _ -> True
