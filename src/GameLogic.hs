74e8765a311271fa843d126a57588fd863e2b3eb
George Spanos <gespa11019@gmail.com>
2026-04-23 14:13:38 +0300

    enhance game state

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -10,5 +10,5 @@ walkable Wall = False
 dirVector :: Direction -> (Int, Int)
 dirVector DirRight = (1, 0)
 dirVector DirLeft = (-1, 0)
-dirVector DirUp = (0, 1)
-dirVector DirDown = (0, -1)
+dirVector DirUp = (0, -1)
+dirVector DirDown = (0, 1)
--- src/Types.hs
+++ src/Types.hs
@@ -1,6 +1,14 @@
 module Types where 
 
+import Data.Map.Strict
+
 data Tile = Wall | Floor | Door deriving (Eq, Show)
 
 data Direction = DirUp | DirDown | DirRight | DirLeft deriving (Eq, Show)
 
+data Coord = Coord {x :: Int, y :: Int} deriving (Eq, Show, Ord)
+
+data GameState = GameState
+  { dungeon :: Map Coord Tile
+  , playerPos  :: Coord
+  } deriving (Eq, Show)
