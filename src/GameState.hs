0400a846db3872c776bde03c0bc3a91ea90aae08
George Spanos <gespa11019@gmail.com>
2026-04-30 22:51:07 +0300

    working on game state

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,10 +1,19 @@
-module GameState where
+module GameState (
+  GameState,
+  mkGameState,
+) where
 
 import Coord
 import Tile
 import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 
 data GameState = GameState
   { dungeon :: Map Coord Tile
   , playerPos  :: Coord
   } deriving (Eq, Show)
+
+mkGameState :: Map Coord Tile -> Coord -> Maybe GameState
+mkGameState dungeon playerPos = case Map.lookup playerPos dungeon of
+  Just t | walkable t -> Just $ GameState dungeon playerPos
+  _ -> Nothing
