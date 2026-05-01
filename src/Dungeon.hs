653631d9176e26cac2000c19b0b2b6844a733b71
George Spanos <gespa11019@gmail.com>
2026-05-01 10:48:24 +0300

    add init game

--- src/Dungeon.hs
+++ src/Dungeon.hs
@@ -1,8 +1,9 @@
-module Dungeon where
+module Dungeon (mkDungeon ) where 
 
 import Coord
 import Tile
 import Data.Map.Strict (Map, fromList)
 
 mkDungeon:: Int -> Int -> Map Coord Tile
-mkDungeon w h = fromList [ (Coord x y, t) | x <- [0..w-1], y <- [0..h -1], let t = if x ==0 || x == w -1 || y == 0 || y == h - 1 then Wall else Floor]
+mkDungeon w h = fromList [ (Coord x y, t) | x <- [0..w-1], y <- [0..h-1], let t = if x ==0 || x == w-1 || y == 0 || y == h-1 then Wall else Floor]
+
--- src/GameState.hs
+++ src/GameState.hs
@@ -1,12 +1,16 @@
 module GameState (
   GameState,
   mkGameState,
+  initGame
 ) where
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromJust)
 
 import Coord
 import Tile
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Dungeon
 
 data GameState = GameState
   { dungeon :: Map Coord Tile
@@ -17,3 +21,9 @@ mkGameState :: Map Coord Tile -> Coord -> Maybe GameState
 mkGameState dungeon playerPos = case Map.lookup playerPos dungeon of
   Just t | walkable t -> Just $ GameState dungeon playerPos
   _ -> Nothing
+
+initGame:: Int -> Int -> GameState
+initGame x y = fromJust $ mkGameState dungeon $ Coord x y 
+  where w = 80
+        h = 24
+        dungeon = mkDungeon w h
