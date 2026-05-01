76643049cca5decbe665d4d859247c1a923a6c7a
George Spanos <gespa11019@gmail.com>
2026-05-01 10:35:06 +0300

    wip dugngeon generate

---
+++ src/Dungeon.hs
@@ -0,0 +1,8 @@
+module Dungeon where
+
+import Coord
+import Tile
+import Data.Map.Strict (Map, fromList)
+
+mkDungeon:: Int -> Int -> Map Coord Tile
+mkDungeon w h = fromList [ (Coord x y, t) | x <- [0..w-1], y <- [0..h -1], let t = if x ==0 || x == w -1 || y == 0 || y == h - 1 then Wall else Floor]
